02301c2ab6225f2da751efbdc68625f814f5e285
moeshin <764686875@qq.com>
2020-05-11 01:20:59 +0800

    build: release 2.0.2

--- QPlayer.js
+++ QPlayer.js
@@ -33,7 +33,7 @@ window.QPlayer.init = function () {
 
     var $lyricsList, $listLi, isLoadPause, isPrevisionPlay, errorStartIndex, isAllError, setCoverTime;
 
-    q.version = '2.0.1';
+    q.version = '2.0.2';
     q.audio = audio;
     q.defaultProvider = q.defaultProvider || 'default';
     q.provider = q.provider || {};
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+uglifyjs QPlayer.js -m -o QPlayer.min.js
+uglifycss QPlayer.css --output QPlayer.min.css

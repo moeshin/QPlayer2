96437e05d3c9de3e1fb1636677782a190dee47e7
MoeShin <hi@moeshin.com>
2021-02-21 04:39:16 +0800

    build: release 2.0.8

--- QPlayer.js
+++ QPlayer.js
@@ -33,7 +33,7 @@ window.QPlayer.init = function () {
 
     var $lyricsList, $listLi, isLoadPause, isPrevisionPlay, errorStartIndex, isAllError, setCoverTime, loadedList;
 
-    q.version = '2.0.7';
+    q.version = '2.0.8';
     q.audio = audio;
     q.defaultProvider = q.defaultProvider || 'default';
     q.provider = q.provider || {};

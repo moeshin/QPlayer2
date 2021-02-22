d21eb92cba059c9ac384f064106dfb85421e0683
MoeShin <hi@moeshin.com>
2021-02-23 02:39:10 +0800

    refactor: use npm

--- QPlayer.js
+++ QPlayer.js
@@ -1,23 +1,93 @@
 var $ = require('jquery');
-// noinspection JSValidateTypes
 require('jquery.simplemarquee');
 var Cookies = require('js-cookie');
 var md5 = require('blueimp-md5');
-var CACHE_LIST_REGEX = /QPlayer-[a-z\d]{32}/;
-if (!window.QPlayer) {
-    window.QPlayer = {};
+var fs = require('fs');
+
+var
+    CACHE_LIST_REGEX = /QPlayer-[a-z\d]{32}/,
+    PLAYING_KEY = getLocalStorageName('playing'),
+    COVER_TIMEOUT = 500
+;
+
+function s2m(s) {
+    var m = Math.floor(s / 60);
+    s = Math.floor(s % 60);
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (s < 10) {
+        s = '0' + s;
+    }
+    return m + ':' + s;
 }
-QPlayer.$ = $;
-window.QPlayer.init = function () {
+
+function getTime() {
+    return new Date().getTime();
+}
+
+function getLocalStorageName(name) {
+    return 'QPlayer-' + name;
+}
+
+function setStringFromLocalStorage(name, value) {
+    localStorage.setItem(getLocalStorageName(name), value);
+}
+
+if (Cookies.get('QPlayer') === undefined) {
+    // clean cache
+    var length = localStorage.length;
+    for (var i = 0; i < length; i++) {
+        var key = localStorage.key(i);
+        if (CACHE_LIST_REGEX.test(key)) {
+            localStorage.removeItem(key);
+        }
+    }
+    localStorage.removeItem(getLocalStorageName('playing'));
+    Cookies.set('QPlayer', '', {
+        path: '/',
+        sameSite: 'strict'
+    });
+}
+
+var q = window.QPlayer = $.extend(true, {
+    defaultProvider: 'default',
+    provider: {
+        default: {
+            dataType: '*',
+            lyrics: function (current, success) {
+                var url = current.lrc;
+                if (!url) {
+                    return;
+                }
+                var dataType = this.dataType;
+                $.ajax({
+                    url: url,
+                    dataType: dataType,
+                    success: function (lrc) {
+                        if (!lrc) {
+                            return;
+                        }
+                        success(lrc);
+                    }
+                });
+            }
+        }
+    }
+}, window.QPlayer || {});
+q = $.extend(q, {
+    $: $,
+    version: process.env.npm_package_version
+});
+q.init = function () {
 
     if (document.getElementById('QPlayer')) {
         return;
     }
 
-    var q = window.QPlayer;
     var v = {};
 
-    $('body').append('<div id="QPlayer"><div id="QPlayer-body"><div id="QPlayer-panel"><div id="QPlayer-cover"><svg id="QPlayer-cover-default" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32" cy="32" r="32" style="fill:#222"/><circle cx="32" cy="32" r="16"/><circle cx="32" cy="32" r="12" style="fill:#e12"/><circle cx="32" cy="32" r="1.2" style="fill:#fff"/></svg></div><div id="QPlayer-control"><div id="QPlayer-title"><strong id="QPlayer-name"></strong> -<span id="QPlayer-artist"></span></div><div id="QPlayer-progress"><div id="QPlayer-progress-current" style="width: 0;"></div></div><div id="QPlayer-control-bar"><div id="QPlayer-time">00:00</div><div id="QPlayer-control-play"><svg id="QPlayer-btn-previous" xmlns="http://www.w3.org/2000/svg" viewBox="6 6 12 12"><path d="M6 6h2v12H6zm3.5 6l8.5 6V6z"/></svg><svg id="QPlayer-btn-next" xmlns="http://www.w3.org/2000/svg" viewBox="6 6 12 12"><path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/></svg><div id="QPlayer-btn-play"><svg id="QPlayer-play" xmlns="http://www.w3.org/2000/svg" viewBox="16 10 22 28"><path d="M16 10v28l22-14z"/></svg><svg id="QPlayer-pause" xmlns="http://www.w3.org/2000/svg" viewBox="12 10 24 28"><path d="M12 38h8V10h-8v28zm16-28v28h8V10h-8z"/></svg></div></div><div id="QPlayer-control-more"><div id="QPlayer-btn-mode"><svg id="QPlayer-btn-repeat" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z"/></svg><svg id="QPlayer-btn-shuffle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z"/></svg></div><svg id="QPlayer-btn-list" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M7 10h9V8H7v2zm0-7v2h9V3H7zm0 12h9v-2H7v2zm-4-5h2V8H3v2zm0-7v2h2V3H3zm0 12h2v-2H3v2z"/></svg><svg id="QPlayer-btn-lyrics" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M4 14v2h10v-2H4zm0-8v2h10V6H4zm-2 6h14v-2H2v2zM2 2v2h14V2H2z"/></svg></div></div></div></div><div id="QPlayer-switch"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg></div></div><div id="QPlayer-more"><ol id="QPlayer-list"></ol><div id="QPlayer-lyrics"></div></div><audio id="QPlayer-audio"></audio></div>');
+    $('body').append(fs.readFileSync('./QPlayer.htm', 'utf8'));
 
     var
         $q = $('#QPlayer'),
@@ -30,58 +100,14 @@ window.QPlayer.init = function () {
         $progress = $('#QPlayer-progress'),
         $progressCurrent = $('#QPlayer-progress-current'),
         $lyrics = $('#QPlayer-lyrics'),
-        $mode = $('#QPlayer-btn-mode')
-    ;
-
-    var
-        audio = $audio[0],
-        coverTimeout = 500,
-        playingKey = getLocalStorageName('playing')
+        $mode = $('#QPlayer-btn-mode'),
+        audio = $audio[0]
     ;
 
     var $lyricsList, $listLi, isLoadPause, isPrevisionPlay, errorStartIndex, isAllError, setCoverTime, loadedList,
         listLocalStorageName, playTime;
 
-    q.version = '2.0.8';
     q.audio = audio;
-    q.defaultProvider = q.defaultProvider || 'default';
-    q.provider = q.provider || {};
-    q.provider.default = q.provider.default || {
-        dataType: '*',
-        lyrics: function (current, success) {
-            var url = current.lrc;
-            if (!url) {
-                return;
-            }
-            var dataType = this.dataType;
-            $.ajax({
-                url: url,
-                dataType: dataType,
-                success: function (lrc) {
-                    if (!lrc) {
-                        return;
-                    }
-                    success(lrc);
-                }
-            });
-        }
-    };
-
-    if (Cookies.get('QPlayer') === undefined) {
-        // clean cache
-        var length = localStorage.length;
-        for (var i = 0; i < length; i++) {
-            var key = localStorage.key(i);
-            if (CACHE_LIST_REGEX.test(key)) {
-                localStorage.removeItem(key);
-            }
-        }
-        localStorage.removeItem(getLocalStorageName('playing'));
-        Cookies.set('QPlayer', '', {
-            path: '/',
-            sameSite: 'strict'
-        });
-    }
 
     function Shuffle(index) {
         var _this = this;
@@ -184,18 +210,6 @@ window.QPlayer.init = function () {
         $q.removeClass('QPlayer-playing');
         $title.simplemarquee('pause');
         removePlaying();
-    }
-
-    function s2m(s) {
-        var m = Math.floor(s / 60);
-        s = Math.floor(s % 60);
-        if (m < 10) {
-            m = '0' + m;
-        }
-        if (s < 10) {
-            s = '0' + s;
-        }
-        return m + ':' + s;
     }
 
     function getListLi(index) {
@@ -431,12 +445,12 @@ window.QPlayer.init = function () {
 
     function initCover() {
         setTimeout(function () {
-            if (setCoverTime >= getTime() - coverTimeout) {
+            if (setCoverTime >= getTime() - COVER_TIMEOUT) {
                 return;
             }
             $cover.css('background-image', '');
             $cover.addClass('QPlayer-cover-no');
-        }, coverTimeout);
+        }, COVER_TIMEOUT);
     }
 
     function initLoad() {
@@ -815,7 +829,7 @@ window.QPlayer.init = function () {
             if (!isProgressClicked) {
                 $progressCurrent.width(100 * time / audio.duration + '%');
             }
-            var playing = localStorage.getItem(playingKey);
+            var playing = localStorage.getItem(PLAYING_KEY);
             if (playing === null) {
                 updatePlaying();
             } else if (playing !== playTime) {
@@ -902,26 +916,14 @@ window.QPlayer.init = function () {
 
     initNoSongs();
 
-    function getTime() {
-        return new Date().getTime();
-    }
-
-    function getLocalStorageName(name) {
-        return 'QPlayer-' + name;
-    }
-
-    function setStringFromLocalStorage(name, value) {
-        localStorage.setItem(getLocalStorageName(name), value);
-    }
-
     function updatePlaying() {
-        localStorage.setItem(playingKey, playTime);
+        localStorage.setItem(PLAYING_KEY, playTime);
     }
 
     function removePlaying() {
-        var playing = localStorage.getItem(playingKey);
+        var playing = localStorage.getItem(PLAYING_KEY);
         if (playing !== null && playing === playTime) {
-            localStorage.removeItem(playingKey);
+            localStorage.removeItem(PLAYING_KEY);
         }
     }
 
@@ -1042,7 +1044,7 @@ window.QPlayer.init = function () {
                     q.load(index);
                 }
                 loadedList = true;
-                if (q.isAutoplay && localStorage.getItem(playingKey) === null) {
+                if (q.isAutoplay && localStorage.getItem(PLAYING_KEY) === null) {
                     q.play();
                 }
             },
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-uglifyjs QPlayer.js -m -o QPlayer.min.js
-uglifycss QPlayer.css --output QPlayer.min.css
